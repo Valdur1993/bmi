1552f04e246f359e993e1d4fe36f375ccd3f0824
Marichka <morozovamg@bk.ru>
2024-11-22 22:11:20 +0700

    tip

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
 
-    public double calculate(double weight, double height) {
+    public int calculate(double weight, double height) {
         double result = weight / (height * height);
-        return result;
+        return (int) result;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 98; //кг
-        double height = 1.87; //м
+        double weight = 80; //кг
+        double height = 1.70; //м
         double BodyMassIndex = (int) service.calculate(weight, height);
         System.out.println("Индекс массы тела : " + (int) BodyMassIndex + "кг/㎡");
     }
